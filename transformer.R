1356141bd681791e307e3a1176f6874421ae565d
alex <alexe_webmail@yahoo.com>
2018-05-03 16:48:40 -0400

    Add gsflow_nws.exe to PRMS module; enhancements to transformer.

--- transformer.R
+++ transformer.R
@@ -2,7 +2,65 @@
 # Copyright © 2007 - 2018 Apex Resource Management Solution Ltd.(ApexRMS) . All rights reserved.
 
 require(methods)
+library(rgdal)
+library(raster)
 library(rsyncrosim)
 
-scen = scenario()
-stsimScenario = datasheet(scen, name = "PRMS_InputSTSimScenario")
+GetDataSheet <- function(name, scen){
+  ds = datasheet(scen, name)
+  if (NROW(ds)==0){stop(paste0("No data for: ", name))}
+  return(ds)
+}
+
+GetSingleValue <- function(ds, name){
+  v = ds[,name]
+  if (is.na(v)) {stop(paste0("Missing data for: ", name))}
+  return (v)
+}
+
+GetFile <- function(ds, name){
+  f = GetSingleValue(ds, name)
+  if (!file.exists(f)){stop(paste0("The file does not exist: ", f))}
+  return(f)
+}
+
+ProcessTemplate = function(templateFile, outputFile) {
+  
+  f1 = file(templateFile, "r")
+  f2 = file(outputFile, "w")
+  
+  while (TRUE){
+    
+    line = readLines(f1, n = 1)
+    
+    if ( length(line) == 0 ) {
+      break
+    }
+    
+    writeLines(line, f2)
+  }
+  
+  close(f1)
+  close(f2)
+}
+
+Scen = scenario()
+Env = ssimEnvironment()
+RunControl = GetDataSheet("PRMS_RunControl", Scen)
+STSimInput = GetDataSheet("PRMS_InputSTSimScenario", Scen)
+
+MaxIteration = GetSingleValue(RunControl, "MaximumIteration")
+MinIteration = GetSingleValue(RunControl, "MinimumIteration")
+MinTimestep = GetSingleValue(RunControl, "MinimumTimestep")
+MaxTimestep = GetSingleValue(RunControl, "MaximumTimestep")
+
+LibraryFile = GetFile(STSimInput, "STSimLibraryFile")
+ResultScenarioID = GetSingleValue(STSimInput,"ResultScenarioID")
+
+for (Iteration in MinIteration:MaxIteration) {
+  
+  for (Timestep in MinTimestep:MaxTimestep) {
+    
+  }
+}
+

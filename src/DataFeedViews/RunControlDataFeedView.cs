6312978ec7a3e32a9a020fcbaf1e03c2ce10cd8e
alex <alexe_webmail@yahoo.com>
2019-02-01 17:14:51 -0800

    Update file header blocks to match readme.md.

--- src/DataFeedViews/RunControlDataFeedView.cs
+++ src/DataFeedViews/RunControlDataFeedView.cs
@@ -1,4 +1,4 @@
-﻿// PRMS: A SyncroSim Package for running PRMS simulations on ST-Sim outputs.
+﻿// stsim-prms: SyncroSim Add-On Package (to stsim) that supports the integration of the PRMS hydrologic model with ST-Sim.
 // Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 
 using SyncroSim.Core;
